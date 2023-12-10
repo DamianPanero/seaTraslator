15cc03963109484415359005b799159dc3e9b8f2
Damian <damigp2@gmail.com>
2023-12-10 11:15:53 -0300

    clean up

--- resources/script/main.js
+++ resources/script/main.js
@@ -1,20 +1,4 @@
-/*const formElement=document.getElementById("myForm");
-let arr=[];
-function translator(evt){
-    evt.preventDefault();
-const value=document.getElementById("myInput").value;
-arr=value.split(" ");
-console.log(arr);
-for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr.length;j++){
-        
-       vocals=(arr[i][j].join("")).match(/[aeiou]/gi) ;
-    }
-}
- return alert(vocals); 
-}
 
-formElement.addEventListener("submit", translator);*/
 const formElement = document.getElementById("myForm");
 
 function translator(evt) {
